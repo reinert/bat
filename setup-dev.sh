2959727ffb7f5994e73b8ebcb5a4434a74a35953
Danilo Reinert <daniloreinert@gmail.com>
2020-10-05 19:00:17 -0300

    Fix build variable names

--- setup-dev.sh
+++ setup-dev.sh
@@ -3,7 +3,7 @@ mkdir .data/pgsql
 mkdir .data/sqlite
 cp .env-template .env
 sed -i "s:\$USR:$USER:g" .env
-UID=`id -u`
-GID=`id -g`
-sed -i "s:\$UID:$UID:g" .env
-sed -i "s:\$GID:$GID:g" .env
+USRID=`id -u`
+GRPID=`id -g`
+sed -i "s:\$UID:$USRID:g" .env
+sed -i "s:\$GID:$GRPID:g" .env
